a530dee595624943e6df6826734ea7828554c64e
imen17 <95828993+imen17@users.noreply.github.com>
2024-01-30 23:17:24 +0100

    Address entity

---
+++ src/main/java/com/project/khob/domain/entities/Address.java
@@ -0,0 +1,28 @@
+package com.project.khob.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "Address")
+public class Address {
+
+    @Id
+    private Long Id;
+    private String addressLine;
+    private String city;
+    private String governorate;
+
+    @OneToOne (cascade = CascadeType.ALL)
+    @MapsId ("userId")
+    @JoinColumn(name = "userId",referencedColumnName = "userId")
+    private String User;
+}
