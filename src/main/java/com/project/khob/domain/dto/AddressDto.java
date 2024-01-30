f01dc50a91b91fee1b4f1eb5555100d621fcf544
imen17 <95828993+imen17@users.noreply.github.com>
2024-01-30 23:53:30 +0100

    Adress Dto

---
+++ src/main/java/com/project/khob/domain/dto/AddressDto.java
@@ -0,0 +1,19 @@
+package com.project.khob.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AddressDto {
+
+    private Long Id;
+    private String addressLine;
+    private String city;
+    private String governorate;
+    private String User;
+}
